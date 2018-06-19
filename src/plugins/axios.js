769c20dce3c69c983299e2780d53f9b0521db114
gpickin <gpickin@gmail.com>
2018-06-19 13:26:51 -0700

    Setup axios with default baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+
 export default ({ Vue }) => {
   Vue.prototype.$axios = axios
 }
+
+// Here we define a named export
+// that we can later use inside .js files:
+export { axiosInstance }
